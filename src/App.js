34a8c666d45680ff83c1593ddd94afe4038091a0
lloydgapton <lloydgapton123@gmail.com>
2024-02-09 18:45:29 +0100

    updated project files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import './App.css';
-import Navbar from "./Navbar"
-import Hero from "./Hero"
-import Card from "./Card"
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
 import data from "./data"
 
-function App (){
-  const cards = data.map(item=>{
-    return(
-    <Card
-      img={item.coverImg}
-      rating={item.stats.rating}
-      reviewcount={item.stats.reviewCount}
-      location={item.location}
-      title={item.title}
-      price={item.price}
-      />)
-  })
-  return(
-    <div>
-      <Navbar />
-      <Hero />
-      {cards}
-    </div>
-  )
+export default function App() {
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                item={item}
+            />
+        )
+    })        
+    
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <section className="cards-list">
+                {cards}
+            </section>
+        </div>
+    )
 }
-
-export default App;
--- src/Navbar.js
+++
@@ -1,9 +0,0 @@
-import logo from "./airbnb-logo.png"
-function Navbar(){
-    return(
-        <nav>
-            <img src={logo} alt="logo" className="nav--logo"/>
-        </nav>
-    )
-}
-export default Navbar;
